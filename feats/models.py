65529e3eaa63b789645d599f323448d0c2fda3c2
Fedor Zhadaev <fzhadaev@griddynamics.com>
2018-03-06 23:41:18 +0400

    Add ability to parse main requirements

--- feats/models.py
+++ feats/models.py
@@ -13,6 +13,19 @@ class Trait(models.Model):
     name = models.CharField(null=False, max_length=32)
 
 
+class Skill(models.Model):
+    name = models.CharField(null=False, max_length=32)
+
+
+class ClassFeature(models.Model):
+    name = models.CharField(null=False, max_length=32)
+
+
+class Class(models.Model):
+    name = models.CharField(null=False, max_length=32)
+    features = models.ManyToManyField(ClassFeature)
+
+
 class Feat(models.Model):
     ARCANE = "A"
     DIVINE = "D"
@@ -30,18 +43,23 @@ class Feat(models.Model):
     goal = models.TextField()
     completion_benefit = models.TextField()
     suggested_traits = models.ManyToManyField(Trait, blank=True)
-    
+
     req_dex = models.IntegerField(null=True, default=None)
     req_str = models.IntegerField(null=True, default=None)
     req_con = models.IntegerField(null=True, default=None)
     req_int = models.IntegerField(null=True, default=None)
     req_wis = models.IntegerField(null=True, default=None)
     req_cha = models.IntegerField(null=True, default=None)
-    
-    req_bab = models.IntegerField(null=True, default=None)
 
-    req_feats = models.ManyToManyField("self", blank=True)
+    req_bab = models.IntegerField(null=True, default=None)
+    req_lvl = models.IntegerField(null=True, default=None)
+
+    req_feats = models.ManyToManyField("self", blank=True, symmetrical=False)
     req_races = models.ManyToManyField(Race, blank=True)
+    req_skills = models.ManyToManyField(Skill, through="RequiredSkill")
+    req_classes = models.ManyToManyField(Class, blank=True)
+    req_class_features = models.ManyToManyField(ClassFeature, blank=True)
+    req_traits = models.ManyToManyField(Trait, blank=True)
 
     caster_lvl = models.IntegerField(null=True, default=None)
     caster_type = models.CharField(max_length=1, choices=MAGIC_TYPES,
@@ -58,3 +76,10 @@ class Feat(models.Model):
     racial = models.BooleanField(null=False, default=False)
     companion_familiar = models.BooleanField(null=False, default=False)
     multiples = models.BooleanField(null=False, default=False)
+
+
+class RequiredSkill(models.Model):
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE,
+                              related_name='+')
+    feat = models.ForeignKey(Feat, on_delete=models.CASCADE)
+    ranks = models.IntegerField(default=0, null=False)
--- utils/feat_parser.py
+++ utils/feat_parser.py
@@ -1,11 +1,26 @@
-import pandas as pd
 import json
+import re
+
+import pandas as pd
 
 MYTHIC = 'Mythic'
 
+NUMBER_REQ_PATTERNS = {
+    "req_dex": re.compile("Dex (\d+)"),
+    "req_str": re.compile("Str (\d+)"),
+    "req_con": re.compile("Con (\d+)"),
+    "req_int": re.compile("Int (\d+)"),
+    "req_wis": re.compile("Wis (\d+)"),
+    "req_cha": re.compile("Cha (\d+)"),
+    "req_bab": re.compile("[Bb]ase attack bonus ([+-]?\d+)"),
+    "req_lvl": re.compile("[Cc]haracter level (\d+)\w*"),
+    "caster_lvl": re.compile("[Cc]aster level (\d+)\w*")
+}
 
-class FeatEntry(object):
-    pass
+SKILL_REQ_PATTERN = re.compile("(\w+) (\d+) rank[s]?")
+CLASS_FEATURE_PATTERN = re.compile("([\w \d]+) class feature")
+RACIAL_TRAIT_PATTERN = re.compile("([\w \d]+) racial trait")
+CLASS_LEVEL_PATTERN = re.compile("(\d+)\w*-level (\w+)")
 
 
 class FeatParser(object):
@@ -19,7 +34,7 @@ class FeatParser(object):
             tmp = self._convert_record(r)
             flat_dict[tmp['name']] = tmp
 
-        print(json.dumps(flat_dict['Snatch Arrows'], indent=2))
+        print(json.dumps(flat_dict['Power Attack'], indent=2))
 
     def _convert_record(self, record):
         result = dict()
@@ -28,32 +43,61 @@ class FeatParser(object):
                           'completion_benefit']
 
         for f in fields_to_copy:
-            result[f] = record[f]
+            result[f] = record.get(f, None)
 
         if MYTHIC == result['type']:
             result['name'] = "{} ({})".format(result['name'], MYTHIC)
 
-        result['suggested_traits'] = self._comma_split_and_title(
-            record['suggested_traits'])
+        result['suggested_traits'] = self._comma_split(
+            record['suggested_traits'], title=True)
 
         requirements = {
-            "races": self._comma_split_and_title(record['race_name']),
-            "feats": self._comma_split_and_title(record['prerequisite_feats']),
-            "as_text": record['prerequisites'].strip('.')
+            "races": self._comma_split(record.get('race_name', None),
+                                       title=True),
+            "feats": self._comma_split(
+                record.get('prerequisite_feats', None), title=True),
+            "as_text": record.get('prerequisites', "").strip('.')
         }
         result['requirements'] = requirements
+        requirements.update(
+            self._parse_requirements_text(requirements['as_text']))
 
         flag_fields = ['teamwork', 'critical', 'grit', 'style', 'performance',
                        'racial', 'companion_familiar', 'multiples']
-        flags = {x: bool(record[x]) for x in flag_fields}
+        flags = {x: bool(record.get(x, False)) for x in flag_fields}
         result['flags'] = flags
 
         return result
 
-    def _comma_split_and_title(self, string):
+    def _parse_requirements_text(self, req_string):
+        result = {}
+        for s in self._comma_split(req_string):
+            for key, pattern in NUMBER_REQ_PATTERNS.items():
+                m = pattern.match(s)
+                if m:
+                    result[key] = int(m.group(1))
+            m = SKILL_REQ_PATTERN.match(s)
+            if m:
+                result.setdefault("req_skills", {})[m.group(1)] = int(m.group(2))
+            m = CLASS_FEATURE_PATTERN.match(s)
+            if m:
+                result.setdefault("req_class_features", []).append(
+                    m.group(1).title())
+            m = RACIAL_TRAIT_PATTERN.match(s)
+            if m:
+                result.setdefault("req_traits", []).append(
+                    m.group(1).title())
+            m = CLASS_LEVEL_PATTERN.match(s)
+            if m:
+                result["req_lvl"] = m.group(1)
+                result.setdefault("req_classes", []).append(m.group(2).title())
+        return result
+
+    def _comma_split(self, string, title=False):
         if not string:
             return []
-        return [p.strip().title() for p in string.strip('.').split(',')]
+        return [p.strip().title() if title else p.strip()
+                for p in string.strip('.').split(',')]
 
 
 if __name__ == '__main__':
